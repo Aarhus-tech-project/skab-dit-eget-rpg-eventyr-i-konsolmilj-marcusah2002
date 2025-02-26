50bec0fde8ae794e373d85332d69570e0c3e7354
Marcus Hansen <marcus.han.mh@gmail.com>
2025-02-26 10:05:19 +0100

    character class

---
+++ RPG spil/Character.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+public class Character
+{
+	//Fields
+	private string name;
+	private int health;
+	private int atttackDamage;
+	private int defense;
+
+	//Constructor
+	public Character(string name, int health, int attackDamage, int defense)
+	{
+		_name = name;
+		_health = health;
+		_attackDamage = attackDamage;
+		_defence = defense;
+
+
+	}
+
+	//Methods
+	public int Attack()
+	{
+		return attackDamage;
+	}
+
+	public void TakeDamage(int damage)
+	{
+		int actualDamage = damage - _defense;
+		if (actualDamage < 0)
+		{
+			actualDamage = 0;
+		}
+
+		_health -= actualDamage;
+		Console.WriteLine($"{_name} takes {actualDamage} damage! Health left: {_health}");
+
+		if (_health <= 0)
+		{
+			Console.WriteLine($"{_name} has been defeated");
+		}
+	}
+}
