0dff083019d9bef4b457337789d0c06fe63505df
Marcus Hansen <marcus.han.mh@gmail.com>
2025-02-27 09:41:52 +0100

    lavet location og gameworld classes

---
+++ RPG spil/GameWorld.cs
@@ -0,0 +1,37 @@
+﻿using System;
+namespace RPG_spil
+{
+
+
+    public class GameWorld
+    {
+
+        private Dictionary<string, Location> _locations;
+        public GameWorld()
+        {
+            _locations = new Dictionary<string, Location>
+          {
+            {"City", new Location("The City", "a lively city filled regular people.") },
+            {"Forest", new Location("The Forest", "is unpredictable and dangerous. Be careful!") }
+          };
+
+
+        }
+
+        public void Travel(string locationName)
+        {
+            if (_locations.ContainsKey(locationName))
+            {
+                _locations[locationName].Enter();
+            }
+            else
+            {
+                Console.WriteLine("That place doesn't exist");
+            }
+        }
+
+    }
+
+}
+
+
---
+++ RPG spil/Location.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RPG_spil
+{
+    class Location
+    {
+        public string Name { get; set; }
+        public string Description { get;}
+
+
+        public Location(string name, string description)
+        {  
+            Name = name; 
+            Description = description; 
+        }
+
+        public void Enter()
+        {
+            Console.WriteLine($"You arrived at {Name}");
+            Console.WriteLine($"{Description}");
+        }
+
+    }
+}
